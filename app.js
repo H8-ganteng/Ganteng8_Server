0ca6263884b899439efe59790520ad679d470a4c
alkindi <kinyobi@pop-os.localdomain>
2021-02-18 23:52:19 +0700

    set up socket

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ if (process.env.NODE_ENV === 'development') {
 
 const express = require('express')
 const app = express()
+
+const http = require('http').Server(app)
+const io = require('socket.io', (socket)=>{
+    console.log('a user connected')
+})
+
 var cors = require('cors')
 const routes = require('./routes/index')
 const port = process.env.PORT || 3000
@@ -17,3 +23,7 @@ app.use(routes)
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
+
+// http.listen(port, ()=>{
+//     console.log('server started')
+// })
