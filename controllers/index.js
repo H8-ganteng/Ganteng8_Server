eeaae5f8abbda852de0554e13900476b33ca1e6c
Mas Agung <57654160+Agung78@users.noreply.github.com>
2021-02-18 16:18:19 +0700

    tambah controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,39 @@
+const { User, Question } = require('../models')
+
 class Controller {
     static addUser(req, res, next) {
-
+        const { username, email, password } = req.body
+        const newUser = { username, email, password }
+        User.findOne({
+            where: {
+                email: newUser.email
+            }
+        }).then(data => {
+            if (data === null) {
+                return User.create(newUser)
+            }
+            if (data.password !== newUser.password) {
+                let compare = false
+                throw compare
+            }
+        }).then(success => {
+            res.status(201).json(success)
+        }).catch(err => {
+            if (err === false) {
+                res.status(404).json({ msg: 'Wrong email/password' })
+            } else {
+                res.status(500).json({ msg: 'Internal server error' })
+            }
+        })
     }
     static getQuestions(req, res, next) {
-
+        Question.findAll()
+            .then(data => {
+                res.status(200).json(data)
+            })
+            .catch(err => {
+                res.status(500).json({ msg: 'Internal server error' })
+            })
     }
 }
 module.exports = Controller
