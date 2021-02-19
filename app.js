c1212d13fed63b980be5d60d78cf1e1f8f39c23d
xavier <anggaez117@gmail.com>
2021-02-19 13:57:42 +0700

    need updated score

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ if (process.env.NODE_ENV === 'development') {
 
 const express = require('express')
 const app = express()
-var cors = require('cors')
+const cors = require('cors')
 const routes = require('./routes/index')
-const port = process.env.PORT || 3000
+const http = require('http').Server(app)
 
 app.use(cors())
 app.use(express.json())
@@ -14,6 +14,4 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use(routes)
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+module.exports = http
